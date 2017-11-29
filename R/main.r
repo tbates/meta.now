00b3faf42bff6b2b8425242867d445bb8d436637
Timothy Bates <timothy.c.bates@gmail.com>
2017-11-29 21:19:01 +0000

    store some more notes and road map

--- R/main.r
+++ R/main.r
@@ -1,17 +1,59 @@
-# TODO
-# 1. Upload existing data
-# 	* Get copy
-# 	* create rdata for speed (each release)
-# 	* create rdata for speed (each release)
-# 2. Create analyses
-# 3. Output to pdf
-# 4. update vignette so browsers can see current results
-# 5. Pick required packages
+### =============================
+### =           TODO            =
+### =============================
+
+### ==================
+### = 1. Get it working =
+### ==================
+# 1. # TODO Upload existing data
+# 	1. Add copy of existing excel spreadsheet
+# 	2. create rdata for speed (each release)
+# 2. # TODO re-create analyses
+# 	1. Format as knitr outputting to pdf/html
+# 3. Pick required packages
 # 	* https://cran.r-project.org/web/packages/psychmeta/index.html
 
-# ===================
-# = Features to add =
-# ===================
-# 1. update via R? or pushing a new-data file to the github, which we then add to the main file?
-# 1. Credit updaters on new doi-based releases of the paper
-# 	* http://blog.chrisgorgolewski.org/2017/11/sharing-academic-credit-in-open-source.html
+### =====================================
+### =         Features to add           =
+### =====================================
+
+### ==========
+### = 2. Output =
+### ==========
+
+# 1. Vignette so browsers can see current results.
+
+### ===============
+### = 3. Updating =
+### ===============
+
+# 1. Add new data
+# 	1. Push new-data file to the github, which we then add to the main file?
+# 1. Update via an R function (creates git pull request?
+# 	3. Add new results function
+# 	4. Add new columns function
+
+### =============================================================
+### = 4. Credit updaters on new doi-based releases of the paper =
+### =============================================================
+# 1. doi built-in.
+# 2. low-effort: via R or uploading a spreadsheet to github; or web interface.
+# 3. Incentive structure to contribute?
+# 	1. http://blog.chrisgorgolewski.org/2017/11/sharing-academic-credit-in-open-source.html
+# 	2. Add re-order to author list for new data or systematic reviews?
+
+
+### =================================
+### = 5. Extensible for more topics =
+### =================================
+# 1. Unify, or fork
+# 2. Able to select results for different topics.
+# 	1. File--> SQL structure
+# 	2. Use [sqlite](https://cran.r-project.org/web/packages/RSQLite/index.html)?
+			# dbGetQuery(db, "SELECT * FROM GxSES WHERE age > 18")
+
+### =================
+### = Systematicity =
+### =================
+# 1. Create system to store, document, and run automated searches on g scholar/pmed and detect candidate new papers
+# 2. Checking/adding for credit/authorship/money if grant or philanthropy available?
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Meta analysis package for GxSES
+# Updatable Meta-analysis package
+
+1. Initially for GxSES.
+2. Updating + verifiability/history via git.
+
+Much more to come.

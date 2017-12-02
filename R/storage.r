6d08f9313fde96d62585206042a9f532e5a60e5f
Timothy Bates <timothy.c.bates@gmail.com>
2017-12-02 11:56:48 +0000

    no message

---
+++ R/storage.r
@@ -0,0 +1,9 @@
+### ==================
+### = Storage =
+### ==================
+# 1. TODO Read and write spreadsheet input
+# 2. TODO update mechanism via github pull requests
+# 	2. implement file interface
+# 3. TODO create and SQL tables for data?
+# 	1. https://cran.r-project.org/web/packages/RSQLite/RSQLite.pdf
+#
